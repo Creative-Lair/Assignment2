4d65d4bea45b19404be97769ea8eb1f117b8e2e0
Ahsan Sohail <k142109@nu.edu.pk>
2017-10-25 21:47:31 +0500

    generate table

--- Assignment5/app/src/main/java/com/example/ahsan/assignment5/Fragment/Table.java
+++ Assignment5/app/src/main/java/com/example/ahsan/assignment5/Fragment/Table.java
@@ -1,12 +1,31 @@
 package com.example.ahsan.assignment5.Fragment;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.ahsan.assignment5.AstronomicalCalendar;
+import com.example.ahsan.assignment5.GeoLocation;
+import com.example.ahsan.assignment5.Preference;
 import com.example.ahsan.assignment5.R;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.List;
+import java.util.TimeZone;
 
 /**
  * Created by AHSAN on 10/25/2017.
@@ -15,11 +34,27 @@ import com.example.ahsan.assignment5.R;
 public class Table extends Fragment implements View.OnClickListener {
     /** Called when the activity is first created. */
 
+    TextView cityname,sunrise,sunset,date;
 
+    Date sd,ed;
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        View view = inflater.inflate(R.layout.frag_main, container, false);
+        View view = inflater.inflate(R.layout.frag_table, container, false);
+        DatePicker start = (DatePicker) view.findViewById(R.id.start);
+        DatePicker end = (DatePicker) view.findViewById(R.id.end);
+        Calendar cal = Calendar.getInstance();
+        int year = cal.get(Calendar.YEAR);
+        int month = cal.get(Calendar.MONTH);
+        int day = cal.get(Calendar.DAY_OF_MONTH);
+
+        cityname = (TextView) view.findViewById(R.id.cname);
+        start.init(year,month,day,dateChangeHandler); // setup initial values and reg. handler
+        end.init(year,month,day,dateChangeHandler);
+        cal.set(Calendar.YEAR, year);
+        cal.set(Calendar.MONTH,month);
+        cal.set(Calendar.DAY_OF_MONTH,day);
+        updateTime(cal.getTime(),cal.getTime(),view);
         return view;
     }
 
@@ -28,64 +63,66 @@ public class Table extends Fragment implements View.OnClickListener {
 
     }
 
-//    private void initializeUI(View view)
-//    {
-//        DatePicker dp = (DatePicker) view.findViewById(R.id.datePicker);
-//        Calendar cal = Calendar.getInstance();
-//        int year = cal.get(Calendar.YEAR);
-//        int month = cal.get(Calendar.MONTH);
-//        int day = cal.get(Calendar.DAY_OF_MONTH);
-//
-//        add = (Button) view.findViewById(R.id.addstate);
-//        list =(Button) view.findViewById(R.id.liststate);
-//        cityname = (TextView) view.findViewById(R.id.cname);
-//        dp.init(year,month,day,dateChangeHandler); // setup initial values and reg. handler
-//        updateTime(year, month, day, view);
-//        add.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Intent myIntent = new Intent(getActivity(), UpdateAcitvity.class);
-//                getActivity().startActivity(myIntent);
-//            }
-//        });
-//        list.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Intent myIntent = new Intent(getActivity(), ListActivity.class);
-//                getActivity().startActivity(myIntent);
-//            }
-//        });
-//    }
-//
-//    private void updateTime(int year, int monthOfYear, int dayOfMonth,View view)
-//    {
-//        Preference pre = new Preference(getActivity());
-//        TimeZone tz = TimeZone.getTimeZone(pre.getTZ());
-//        GeoLocation geolocation = new GeoLocation(pre.getNames(), new Double(pre.getLat()), new Double(pre.getLon()), tz);
-//        AstronomicalCalendar ac = new AstronomicalCalendar(geolocation);
-//        ac.getCalendar().set(year, monthOfYear, dayOfMonth);
-//        Date srise = ac.getSunrise();
-//        Date sset = ac.getSunset();
-//        cityname.setText(pre.getNames()+",AU");
-//        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
-//
-//        TextView sunriseTV = (TextView) view.findViewById(R.id.sunriseTimeTV);
-//        TextView sunsetTV = (TextView) view.findViewById(R.id.sunsetTimeTV);
-//        Log.d("SUNRISE Unformatted", srise+"");
-//
-//        sunriseTV.setText(sdf.format(srise));
-//        sunsetTV.setText(sdf.format(sset));
-//    }
-//
-//    DatePicker.OnDateChangedListener dateChangeHandler = new DatePicker.OnDateChangedListener()
-//    {
-//        public void onDateChanged(DatePicker dp, int year, int monthOfYear, int dayOfMonth)
-//        {
-//            updateTime(year, monthOfYear, dayOfMonth,getView());
-//        }
-//    };
-//    @Override
-//    public void onClick(View v) {
-//
-//    }
+    private void updateTime(Date startDate, Date endDate,View view)
+    {
+        Preference pre = new Preference(getActivity());
+        TimeZone tz = TimeZone.getTimeZone(pre.getTZ());
+        GeoLocation geolocation = new GeoLocation(pre.getNames(), new Double(pre.getLat()), new Double(pre.getLon()), tz);
+        AstronomicalCalendar ac = new AstronomicalCalendar(geolocation);
+        sd = startDate;
+        ed = endDate;
+        List<Date> dates = new ArrayList<>();
+        Calendar calendar = new GregorianCalendar();
+        calendar.setTime(sd);
+        cityname.setText(pre.getNames()+",AU");
+        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
+        SimpleDateFormat sdf2 = new SimpleDateFormat("dd-MM-yyyy");
+        TableLayout ll = (TableLayout) view.findViewById(R.id.displayLinear);
+        ll.removeAllViews();
+        while (calendar.getTime().before(ed))
+        {
+            Date result = calendar.getTime();
+            int year = calendar.get(Calendar.YEAR);
+            int month = calendar.get(Calendar.MONTH);
+            int day = calendar.get(Calendar.DAY_OF_MONTH);
+            ac.getCalendar().set(year, month, day);
+            TableRow row = (TableRow) view.inflate(getContext(),R.layout.table_row,null);
+            sunrise = (TextView) row.findViewById(R.id.sunriseTimeTV);
+            sunset = (TextView) row.findViewById(R.id.sunsetTimeTV);
+            date = (TextView) row.findViewById(R.id.date);
+
+            sunrise.setText(sdf.format(ac.getSunrise()));
+            sunset.setText(sdf.format(ac.getSunset()));
+            date.setText(sdf2.format(result));
+            dates.add(result);
+            ll.addView(row);
+            calendar.add(Calendar.DATE, 1);
+        }
+    }
+
+    DatePicker.OnDateChangedListener dateChangeHandler = new DatePicker.OnDateChangedListener()
+    {
+        public void onDateChanged(DatePicker dp, int year, int monthOfYear, int dayOfMonth)
+        {
+            int id = dp.getId();
+            switch (id){
+                case R.id.start:
+                    Calendar cal = new GregorianCalendar();
+                    cal.set(Calendar.YEAR, year);
+                    cal.set(Calendar.MONTH,monthOfYear);
+                    cal.set(Calendar.DAY_OF_MONTH,dayOfMonth);
+                    updateTime(cal.getTime(),ed,getView());
+                    break;
+                case R.id.end:
+                    Calendar cal2 = new GregorianCalendar();
+                    cal2.set(Calendar.YEAR, year);
+                    cal2.set(Calendar.MONTH,monthOfYear);
+                    cal2.set(Calendar.DAY_OF_MONTH,dayOfMonth);
+                    updateTime(sd,cal2.getTime(),getView());
+                    break;
+            }
+
+        }
+    };
+
 }
